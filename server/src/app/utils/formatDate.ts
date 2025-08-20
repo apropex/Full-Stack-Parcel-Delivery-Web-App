ab5760a04e588521bd72e759f2d6915a4595afac
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-08-20 15:20:02 +0600

    Keep only client folder in client branch

--- server/src/app/utils/formatDate.ts
+++
@@ -1,7 +0,0 @@
-export const formatDate = (date: Date | string): string => {
-  return new Intl.DateTimeFormat("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  }).format(new Date(date));
-};
